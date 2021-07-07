825266bd535cbd3802cf31c4f6befb3b5a244914
SergiCB99 <sergicampanalesbalana@gmail.com>
2021-07-07 14:14:59 +0200

    Create Ex9App.java

---
+++ Ex9App.java
@@ -0,0 +1,16 @@
+
+public class Ex9App {
+
+	public static void main(String[] args) {
+		
+		//Recorre todos los numeros del 1 al 100
+		for(int i = 1;i<=100;i++) {
+			//Comprueba si es divisible entre 2 y 3
+			if(i%2==0 && i%3==0) {
+				System.out.println(i);
+			}
+		}
+		
+	}
+	
+}
