0bb6b8b33eca8fc6ac3bd82d46a2403631996cae
SergiCB99 <sergicampanalesbalana@gmail.com>
2021-07-07 14:15:43 +0200

    Create Ex7App.java

---
+++ Ex7App.java
@@ -0,0 +1,18 @@
+
+public class Ex7App {
+
+	public static void main(String[] args) {
+		
+		//Declaro la variable que guardara los numeros
+		int num=1;
+		
+		//Recorre todos los numeros del 1 al 100
+		while(num<=100) {
+			System.out.println(num);
+			//Incremento el numero
+			num++;
+		}
+		
+	}
+	
+}
