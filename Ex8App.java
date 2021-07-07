ad8c9accdfa07a1809b8d63e73210d3218e2940f
SergiCB99 <sergicampanalesbalana@gmail.com>
2021-07-07 14:15:21 +0200

    Create Ex8App.java

---
+++ Ex8App.java
@@ -0,0 +1,12 @@
+
+public class Ex8App {
+
+	public static void main(String[] args) {
+		
+		//Recorre todos los numeros del 1 al 100
+		for(int i = 1;i<=100;i++) {
+			System.out.println(i);
+		}
+		
+	}
+}
