cfc63de71100f06b72151b4e083fddaaf9be2460
SergiCB99 <sergicampanalesbalana@gmail.com>
2021-07-07 14:17:22 +0200

    Create Ex1App.java

---
+++ Ex1App.java
@@ -0,0 +1,15 @@
+
+public class Ex1App {
+
+	public static void main(String[] args) {
+		
+		//Declaro las variables necessarias
+		int A = 7;
+		int B = 5;
+		
+		//Muestro el resultado
+		System.out.println("El numero mas grande es el "+Math.max(A, B));
+		
+	}
+	
+}
