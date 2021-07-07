0c8d14f1383b148965f3607d8e07f9db6d94934c
SergiCB99 <sergicampanalesbalana@gmail.com>
2021-07-07 14:17:14 +0200

    Create Ex2App.java

---
+++ Ex2App.java
@@ -0,0 +1,14 @@
+
+public class Ex2App {
+
+	public static void main(String[] args) {
+		
+		//Declaro la variable nombre
+		String nombre = "Sergi";
+		
+		//Muestro el resultado
+		System.out.println("Binevenido "+nombre);
+		
+	}
+	
+}
